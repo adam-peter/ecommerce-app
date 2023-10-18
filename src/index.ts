1bdad36be03ede4ef2feb1ee49eaf622afe1c391
badmood111 <petrzela.adam@seznam.cz>
2023-10-18 15:30:07 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+#the way we fetch data
+type Query {
+  hello: String
+}
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => {
+      return "Hello! :D";
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
